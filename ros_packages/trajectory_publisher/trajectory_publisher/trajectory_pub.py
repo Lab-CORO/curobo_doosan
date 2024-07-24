84b0ad49d9b80f0627acc9ea91c7ac1f54ecc939
LuCarpentier92 <lucas.carpentier.1@ens.etsmtl.ca>
2024-07-24 16:55:30 -0400

    rviz visu

--- ros_packages/trajectory_publisher/trajectory_publisher/trajectory_pub.py
+++ ros_packages/trajectory_publisher/trajectory_publisher/trajectory_pub.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 
+
 import rclpy
 from rclpy.node import Node
-from moveit_msgs.msg import DisplayTrajectory
+from sensor_msgs.msg import JointState
+from moveit_msgs.msg import DisplayTrajectory, RobotState, RobotTrajectory
 from trajectory_msgs.msg import JointTrajectory, JointTrajectoryPoint
-from std_msgs.msg import Header
+from std_msgs.msg import Header, Float64MultiArray
 import torch
 from curobo.geom.sdf.world import CollisionCheckerType
 from curobo.geom.types import Cuboid, WorldConfig
@@ -21,25 +23,21 @@ class TrajectoryPublisher(Node):
     def __init__(self):
         super().__init__('trajectory_publisher')
         self.publisher_ = self.create_publisher(DisplayTrajectory, 'display_trajectory', 10)
-        self.timer = self.create_timer(1.0, self.timer_callback)  # Publie toutes les secondes
-        self.initialize_trajectory()
+        self.publish_trajectory()
 
-    def initialize_trajectory(self):
-        # Initialiser la configuration et générer la trajectoire
-        self.setup_trajectory()
+    def trajectory_generator(self):
+        # Standard Library
+        # PLOT = True
+        radius = 0.05
+        act_distance = 0.4
+        voxel_size = 0.05
+        render_voxel_size = 0.02
+        clipping_distance = 0.7
+        tensor_args = TensorDeviceType()
 
-    def setup_trajectory(self):
-        # Initialiser les configurations et le générateur de trajectoire
-        self.radius = 0.05
-        self.act_distance = 0.4
-        self.voxel_size = 0.05
-        self.render_voxel_size = 0.02
-        self.clipping_distance = 0.7
-        self.tensor_args = TensorDeviceType()
+        collision_checker_type = CollisionCheckerType.BLOX
 
-        self.collision_checker_type = CollisionCheckerType.BLOX
-
-        self.world_cfg = WorldConfig.from_dict(
+        world_cfg = WorldConfig.from_dict(
             {
                 "blox": {
                     "world": {
@@ -63,11 +61,32 @@ class TrajectoryPublisher(Node):
             }
         )
 
-        self.robot_cfg = load_yaml(join_path(get_robot_configs_path(), "/pkgs/curobo_doosan/src/m1013/m1013.yml"))["robot_cfg"]
-        self.motion_gen_config = MotionGenConfig.load_from_robot_config(
+        tensor_args = TensorDeviceType()
+
+        self.robot_cfg = load_yaml(join_path(get_robot_configs_path(), "/pkgs/curobo_doosan/src/m1013/m1013.yml"))[
+            "robot_cfg"]
+
+        self.j_names = self.robot_cfg["kinematics"]["cspace"]["joint_names"]
+
+        # print(j_names)
+        self.default_config = self.robot_cfg["kinematics"]["cspace"]["retract_config"]
+
+        print(type(self.default_config))
+        # print(default_config)
+
+        world_cfg_table = WorldConfig.from_dict(
+            load_yaml(join_path(get_world_configs_path(), "collision_wall.yml"))
+        )
+
+        world_cfg_table.cuboid[0].pose[2] -= 0.01
+        world_cfg.add_obstacle(world_cfg_table.cuboid[0])
+        world_cfg.add_obstacle(world_cfg_table.cuboid[1])
+
+        # robot_file = "franka.yml"
+        motion_gen_config = MotionGenConfig.load_from_robot_config(
             self.robot_cfg,
-            self.world_cfg,
-            self.tensor_args,
+            world_cfg,
+            tensor_args,
             trajopt_tsteps=32,
             collision_checker_type=CollisionCheckerType.BLOX,
             use_cuda_graph=True,
@@ -84,36 +103,53 @@ class TrajectoryPublisher(Node):
             minimize_jerk=True,
         )
 
-        self.motion_gen = MotionGen(self.motion_gen_config)
-        self.motion_gen.warmup()
+        motion_gen = MotionGen(motion_gen_config)
 
-        self.world_model = self.motion_gen.world_collision
-        realsense_data = RealsenseDataloader(clipping_distance_m=self.clipping_distance)
+        print("warming up..")
+
+        motion_gen.warmup()
+
+        world_model = motion_gen.world_collision
+        realsense_data = RealsenseDataloader(clipping_distance_m=clipping_distance)
         data = realsense_data.get_data()
+
         camera_pose = Pose.from_list([0, 0, 0, 0.707, 0.707, 0, 0])
-        data_camera = CameraObservation(depth_image=data["depth"], intrinsics=data["intrinsics"], pose=camera_pose)
-        data_camera = data_camera.to(device=self.tensor_args.device)
-        self.world_model.add_camera_frame(data_camera, "world")
-        self.world_model.process_camera_frames("world", False)
+
+        data_camera = CameraObservation(  # rgb_image = data["rgba_nvblox"],
+            depth_image=data["depth"], intrinsics=data["intrinsics"], pose=camera_pose
+        )
+
+        data_camera = data_camera.to(device=tensor_args.device)
+        world_model.add_camera_frame(data_camera, "world")
+        world_model.process_camera_frames("world", False)
         torch.cuda.synchronize()
-        self.world_model.update_blox_hashes()
+        world_model.update_blox_hashes()
 
         bounding = Cuboid("t", dims=[1, 1, 1.0], pose=[0, 0, 0, 1, 0, 0, 0])
-        voxels = self.world_model.get_voxels_in_bounding_box(bounding, self.voxel_size)
+        voxels = world_model.get_voxels_in_bounding_box(bounding, voxel_size)
+
         if voxels.shape[0] > 0:
-            voxels = voxels[voxels[:, 2] > self.voxel_size]
+            voxels = voxels[voxels[:, 2] > voxel_size]
             voxels = voxels[voxels[:, 0] > 0.0]
+
             voxels = voxels.cpu().numpy()
 
-        retract_cfg = self.motion_gen.get_retract_config()
-        state = self.motion_gen.rollout_fn.compute_kinematics(JointState.from_position(retract_cfg.view(1, -1)))
+        # motion_gen.warmup(enable_graph=True, warmup_js_trajopt=js, parallel_finetune=True)
+        # robot_cfg = load_yaml(join_path(get_robot_configs_path(), robot_file))["robot_cfg"]
+        # robot_cfg = RobotConfig.from_dict(robot_cfg, tensor_args)
+
+        retract_cfg = motion_gen.get_retract_config()
+        state = motion_gen.rollout_fn.compute_kinematics(
+            JointState.from_position(retract_cfg.view(1, -1))
+        )
+
         retract_pose = Pose(state.ee_pos_seq.squeeze(), quaternion=state.ee_quat_seq.squeeze())
         start_state = JointState.from_position(retract_cfg.view(1, -1))
         goal_pose = Pose.from_list([-0.4, 0.0, 0.4, 1.0, 0.0, 0.0, 0.0])
 
         start_state.position[0, 0] += 0.25
 
-        result = self.motion_gen.plan_single(
+        result = motion_gen.plan_single(
             start_state,
             retract_pose,
             MotionGenPlanConfig(
@@ -124,44 +160,53 @@ class TrajectoryPublisher(Node):
         )
         new_result = result.clone()
         new_result.retime_trajectory(0.5, create_interpolation_buffer=True)
-        self.traj = result.get_interpolated_plan()
 
-    def timer_callback(self):
-        # Créer un message DisplayTrajectory
-        msg = DisplayTrajectory()
-        msg.header = Header(stamp=self.get_clock().now().to_msg(), frame_id='base_link')
-        msg.trajectory = [self.convert_to_joint_trajectory(self.traj)]
-        msg.trajectory_start = self.get_trajectory_start()
+        traj = result.get_interpolated_plan()
 
-        # Publier le message
-        self.publisher_.publish(msg)
-        self.get_logger().info('Publishing trajectory')
+        position = traj.position.cpu().tolist()
 
-    def convert_to_joint_trajectory(self, traj):
-        # Convertir la trajectoire en JointTrajectory
-        trajectory = JointTrajectory()
-        trajectory.joint_names = ['joint1', 'joint2', 'joint3']  # Nommer vos joints ici
+        return position
 
-        for point in traj:
-            traj_point = JointTrajectoryPoint()
-            traj_point.positions = point.positions.tolist()
-            traj_point.time_from_start.sec = int(point.time_from_start)
-            traj_point.time_from_start.nanosec = int((point.time_from_start - int(point.time_from_start)) * 1e9)
-            trajectory.points.append(traj_point)
+    def publish_trajectory(self):
 
-        return trajectory
+        traj = self.trajectory_generator()
 
-    def get_trajectory_start(self):
-        # Créer un message RobotState vide ou avec des valeurs par défaut
-        from moveit_msgs.msg import RobotState
-        start_state = RobotState()
-        return start_state
+        display_traj = DisplayTrajectory()
+        display_traj.model_id = "m1013"
+
+        robot_traj = RobotTrajectory()
+        joint_traj = JointTrajectory()
+        joint_traj.header = Header()
+        joint_traj.header.stamp = self.get_clock().now().to_msg()
+        joint_traj.joint_names = self.j_names
+
+        for pos in traj:
+            point = JointTrajectoryPoint()
+            point.positions = pos
+            joint_traj.points.append(point)
+
+        robot_traj.joint_trajectory = joint_traj
+        display_traj.trajectory.append(robot_traj)
+
+        initial_state = RobotState()
+
+        initial_state.joint_state.position = self.default_config
+
+
+
+        initial_state.joint_state.name = ['joint1', 'joint2', 'joint3', 'joint4', 'joint5', 'joint6']
+        initial_state.joint_state.position = self.default_config
+        display_traj.trajectory_start = initial_state
+
+        self.publisher_.publish(display_traj)
+
+
+
 
 def main(args=None):
     rclpy.init(args=args)
-    trajectory_publisher = TrajectoryPublisher()
-    rclpy.spin(trajectory_publisher)
-    trajectory_publisher.destroy_node()
+    node = TrajectoryPublisher()
+    rclpy.spin(node)
     rclpy.shutdown()
 
 if __name__ == '__main__':
